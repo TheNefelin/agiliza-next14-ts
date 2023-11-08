1462a3103504f8abe2cd0765b8f7ffed65014da1
Francisco Carmona <flcarmonac@yahoo.com>
2023-11-08 19:23:56 -0300

    update

--- src/components/MyFooter.tsx
+++ src/components/MyFooter.tsx
@@ -2,8 +2,11 @@ import Link from "next/link"
 
 function MyFooter() {
   return (
-    <footer className="flex justify-center p-2 bg-black text-white text-sm">
-      <Link href={"https://www.francisco-dev.cl/"} target="_blank">Powered By @Nefelin</Link>
+    <footer className="flex gap-1 justify-center p-4 bg-black text-white text-sm h-auto">
+      <p>Powered By |</p>
+      <Link href={"https://www.francisco-dev.cl/"} target="_blank">@Nefelin</Link>
+      <p>, legacy &#10132;</p>
+      <Link href={"https://www.proceso-agiliza.cl/"} target="_blank">(www)</Link>
     </footer>
   )
 }
